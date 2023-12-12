09f0b70a6ddd69ca271c5bee299cadf26f55e83d
Денис Уцеха <d.utsekha@reactor.com.ua>
2023-12-12 16:16:41 +0200

    feat: update errors

--- task_2.py
+++ task_2.py
@@ -22,15 +22,15 @@ def handle_error(func):
         try:
             return func(*args, **kwargs)
         except PhoneAlreadyExistError:
-            print("Такий телефон уже є в списку")
-        except RecordAlreadyExistError:
-            print("Запис з таким іменем вже створенний.")
-        except RecordNotExistError:
-            print("Такого запису не знайденно.")
+            print("Such a phone already exists")
         except PhoneNotExistError:
-            print("Такого телефону немає в списку.")
+            print("There is no such phone.")
+        except RecordAlreadyExistError:
+            print("A record with this name already exists.")
+        except RecordNotExistError:
+            print("No such record found.")
         except KeyError:
-            return "По такому ключу нічого не знайдено"
+            print("Nothing was found for this key.")
 
     return inner
 
@@ -117,37 +117,3 @@ class AddressBook(UserDict):
     @handle_error
     def delete(self, name):
         del self.data[name]
-
-
-# Створення нової адресної книги
-book = AddressBook()
-
-# Створення запису для John
-john_record = Record("John")
-john_record.add_phone("1234567890")
-john_record.add_phone("5555555555")
-john_record.edit_phone("5555555555123", "123123123123123")
-
-# Додавання запису John до адресної книги
-book.add_record(john_record)
-
-# Створення та додавання нового запису для Jane
-jane_record = Record("Jane")
-jane_record.add_phone("9876543210")
-book.add_record(jane_record)
-
-# Виведення всіх записів у книзі
-for name, record in book.data.items():
-    print(f"record - {record}")
-
-# Знаходження та редагування телефону для John
-john = book.find("John")
-john.edit_phone("1234567890", "1112223333")
-print(john)
-
-# Пошук конкретного телефону у записі John
-found_phone = john.find_phone("5555555555")
-print(f"{john.name}: {found_phone}")
-
-# Видалення запису Jane
-book.delete("Jane")
